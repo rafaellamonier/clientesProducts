8d4535c84df92572a21a9febf2bcb51aa49abd50
rafaellamonier <rafaellamonier17@hotmail.com>
2026-05-18 14:47:46 -0300

    adjusts and add docker-compose-example.yml

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,7 @@ import { errorMiddleware } from "./middlewares/errorMiddleware";
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
-
 app.use(errorMiddleware);
 
 export { app };
